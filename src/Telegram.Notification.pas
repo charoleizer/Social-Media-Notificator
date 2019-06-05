70b0ae9ffe178e2f0bc300700bc2ce704adcdda8
Victor Zanella <victor.t.y.z@hotmail.com>
2019-06-05 03:17:45 -0300

    Steps to implement later

--- src/Telegram.Notification.pas
+++ src/Telegram.Notification.pas
@@ -23,7 +23,17 @@ uses
 
 procedure TTelegramNotification.SendNotification(ASocialMedia: TSocialMedia);
 begin
+  //TODO
 
+  // Must create a new bot with BotFather
+  // /start with user that you want to notify
+  // Test with getMe
+  // Send a manual message first
+  // teste getUpdates to get User ID
+  
+  // Bot Token: 816598649:AAFBT6epXzrhCi8cdn0PrQ8dk-eDOhKtwU4
+  // User Id: 744289911
+  
   MessageBox(0, PWideChar('Message sent by Telegram' +
     #13#10 +
     'From: ' + ASocialMedia.User + #13#10 +
